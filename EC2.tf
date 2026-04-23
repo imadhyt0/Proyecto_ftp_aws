3fffcfc622c2ef89bd252e90fbbd808da29f4fe8
imadhyt <imadhyt0@gmail.com>
2026-04-23 18:37:27 +0200

    fix: Actualización de AMIs y perfil LabInstanceProfile

--- EC2.tf
+++ EC2.tf
@@ -1,26 +1,26 @@
 resource "aws_instance" "srv_ftp" {
-  ami           = "ami-0e2c8ccd9e036d13a" # Ubuntu 24.04
+  ami           = "ami-0ec10929233384c7f" 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.sub_pub_ftp.id
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.sg_ftp.id]
-  iam_instance_profile   = aws_iam_instance_profile.profile_ftp.name
+  
 
-  # ESTA ES LA LÍNEA NUEVA:
-  user_data = file("instalar_ftp.sh")
+  iam_instance_profile = "LabInstanceProfile"
+
+  user_data = file("FTP.sh")
 
   tags = { Name = "Servidor-FTP-Ubuntu" }
 }
 
 resource "aws_instance" "srv_ldap" {
-  ami           = "ami-0533af06891040f3b" # Amazon Linux 2023
+  ami           = "ami-098e39bafa7e7303d" 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.sub_priv_ldap.id
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.sg_ldap.id]
 
-  # ESTA ES LA LÍNEA NUEVA:
-  user_data = file("instalar_ldap.sh")
+  user_data = file("LDAP.sh")
 
   tags = { Name = "Servidor-LDAP-Privado" }
 }
